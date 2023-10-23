0a950cccb1f3229d7f9ce451d1694faa4156a556
Popoola <popoolso@ucmail.uc.edu>
2023-10-23 10:32:17 -0400

    My first commit

---
+++ Lab8/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            browseButton = new Button();
+            submitButton = new Button();
+            exitButton = new Button();
+            searchBox = new TextBox();
+            listBox = new ListBox();
+            SuspendLayout();
+            // 
+            // browseButton
+            // 
+            browseButton.Location = new Point(473, 54);
+            browseButton.Name = "browseButton";
+            browseButton.Size = new Size(94, 29);
+            browseButton.TabIndex = 0;
+            browseButton.Text = "Browse";
+            browseButton.UseVisualStyleBackColor = true;
+            browseButton.Click += browseButton_Click;
+            // 
+            // submitButton
+            // 
+            submitButton.Location = new Point(242, 397);
+            submitButton.Name = "submitButton";
+            submitButton.Size = new Size(94, 29);
+            submitButton.TabIndex = 1;
+            submitButton.Text = "Submit";
+            submitButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            exitButton.Location = new Point(455, 397);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(94, 29);
+            exitButton.TabIndex = 2;
+            exitButton.Text = "Exit";
+            exitButton.UseVisualStyleBackColor = true;
+            exitButton.Click += exitButton_Click;
+            // 
+            // searchBox
+            // 
+            searchBox.Location = new Point(193, 56);
+            searchBox.Name = "searchBox";
+            searchBox.Size = new Size(252, 27);
+            searchBox.TabIndex = 3;
+            // 
+            // listBox
+            // 
+            listBox.FormattingEnabled = true;
+            listBox.ItemHeight = 20;
+            listBox.Location = new Point(242, 112);
+            listBox.Name = "listBox";
+            listBox.Size = new Size(307, 244);
+            listBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox);
+            Controls.Add(searchBox);
+            Controls.Add(exitButton);
+            Controls.Add(submitButton);
+            Controls.Add(browseButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button browseButton;
+        private Button submitButton;
+        private Button exitButton;
+        private TextBox searchBox;
+        private ListBox listBox;
+    }
+}
---
+++ Lab8/Form1.cs
@@ -0,0 +1,63 @@
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                fileName = ofd.FileName;
+                searchBox.Text = fileName.Split("\\").LastOrDefault();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            List<Task> tasks = new List<Task>();
+            var wordTask = ReadBook(fileName);
+            tasks.Add(wordTask);
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task ReadBook(string fileName)
+        {
+            var lines = await Task.FromResult(File.ReadAllLinesAsync(fileName));
+            var linesWithoutPunctuation = lines.Result.Where(x => x != string.Empty)
+                .AsParallel()
+                .Select(x => x.ToLower().Trim().Replace(",", "").Replace("*", "")
+                .Replace(".", ""));
+            var arrayOfWords = linesWithoutPunctuation.SelectMany(x => x.Split(" "));
+            Dictionary<string, int> dictionaryOfWords = new Dictionary<string, int>();
+            foreach (var word in arrayOfWords)
+            {
+                if (dictionaryOfWords.ContainsKey(word))
+                {
+                    dictionaryOfWords[word]++;
+                }
+                else
+                {
+                    dictionaryOfWords.Add(word, 1);
+                }
+            }
+            var top10 = dictionaryOfWords.Where(x => !string.IsNullOrWhiteSpace(x.Key)).OrderByDescending(x => x.Value).Take(10);
+            foreach (var word in top10)
+            {
+                listBox.Items.Add(word.Key + " - " + word.Value);
+            }
+        }
+    }
+}
